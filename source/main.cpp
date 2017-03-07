ebab11bbd93227d7ce69ce6262dabc800fdfd3a1
Jonathan Gallimore <jon@jrg.me.uk>
2017-03-07 22:17:54 +0000

    Including IR library

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,41 @@
-#include "mbed.h"
- 
+#include <mbed.h>
+#include "ReceiverIR.h"
+
 Serial pc(USBTX, USBRX); // tx, rx
-Serial device(p2, p1);  // tx, rx
-int main() {
-    char temp=0;
-    device.baud(2400);
+ReceiverIR ir_rx(p3);
+Ticker ledTicker;
+
+int receive(RemoteIR::Format *format, uint8_t *buf, int bufsiz, int timeout = 100) {
+    int cnt = 0;
+    while (ir_rx.getState() != ReceiverIR::Received) {
+        cnt++;
+        if (timeout < cnt) {
+            return -1;
+        }
+    }
+	
+    return ir_rx.getData(format, buf, bufsiz * 8);
+}
+
+int main(void) {
+
     while (1) {
-        //RF Receive Code
-        if (device.readable()) {
-            temp=device.getc();
-            //Ignore Sync pattern and do not pass on to PC
-            if (temp!=0xAA) pc.putc(temp);
+        uint8_t buf1[32];
+        int bitlength1;
+        RemoteIR::Format format;
+
+        memset(buf1, 0x00, sizeof(buf1));
+
+        {
+            bitlength1 = receive(&format, buf1, sizeof(buf1));
+            if (bitlength1 < 0) {
+                continue;
+            }
+		    
+			const int n = bitlength1 / 8 + (((bitlength1 % 8) != 0) ? 1 : 0);
+		    for (int i = 0; i < n; i++) {
+		        pc.printf("%02X", buf1[i]);
+		    }
         }
     }
 }
